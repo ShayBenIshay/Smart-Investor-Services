27078dbc918f8fd11e56619babd55b9c3a9275a5
Shay <shaybishay@gmail.com>
2024-12-10 08:42:52 +0200

    temporery using explicit url

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -3,11 +3,11 @@ import { MongoClient } from 'mongodb'
 import type { Db } from 'mongodb'
 import type { Application } from './declarations'
 
-const mongodbUri = process.env.MONGODB_URI
+// const mongodbUri = process.env.MONGODB_URI
 
-if (!mongodbUri) {
-  throw new Error('MONGODB_URI environment variable is not set')
-}
+// if (!mongodbUri) {
+//   throw new Error('MONGODB_URI environment variable is not set')
+// }
 
 declare module './declarations' {
   interface Configuration {
@@ -16,8 +16,10 @@ declare module './declarations' {
 }
 
 export const mongodb = (app: Application) => {
-  const database = new URL(mongodbUri).pathname.substring(1)
-  const mongoClient = MongoClient.connect(mongodbUri).then((client) => client.db(database))
+  const connection = app.get('mongodb') as string
+
+  const database = new URL(connection).pathname.substring(1)
+  const mongoClient = MongoClient.connect(connection).then((client) => client.db(database))
 
   app.set('mongodbClient', mongoClient)
 }
